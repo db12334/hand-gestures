d6f7e30f694c8d2011004cb10d11b782a6b2b4a3
db12334 <118720999+db12334@users.noreply.github.com>
2024-04-10 17:30:29 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-https://teachablemachine.withgoogle.com/models/nNiF7u4zx/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/nNiF7u4zx/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
